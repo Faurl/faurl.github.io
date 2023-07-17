1317d1f7e113bc86aa6221cb9e06bd4eb4412fd3
Faurl852 <113548770+Faurl@users.noreply.github.com>
2023-07-17 13:47:50 +0200

    Delete yt-msplayer-preview.php

--- musicplayer/yt-msplayer-preview.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo '<meta property="og:title" content="Reproductor de Música by Faurl852">';
-echo '<meta property="og:description" content="Reproductor de PlayLists de YouTube mediante Url">';
-echo '<meta property="og:image" content="https://faurl.github.io/musicplayer/preview1.png">';
-?>
