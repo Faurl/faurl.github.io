915ba9334df78d7e64f0b860968e852abb0a5b10
Faurl852 <113548770+Faurl@users.noreply.github.com>
2023-07-17 13:24:38 +0200

    Update yt-msplayer-preview.php

--- musicplayer/yt-msplayer-preview.php
+++ musicplayer/yt-msplayer-preview.php
@@ -1,4 +1,3 @@
-<?php
 $param = $_GET['playlist']; // Obtener el valor del parámetro de la URL
 
 if ($param == 'playlist') {
@@ -12,9 +11,15 @@ if ($param == 'playlist') {
       $playlistId = $matches[1];
   }
 
+  // Imprimir el valor de $playlistId para verificar si se asigna correctamente
+  echo "Playlist ID: " . $playlistId . "<br>";
+
   if (!empty($playlistId)) {
       // Hacer una solicitud a la API de YouTube para obtener la información de la lista de reproducción
       $apiUrl = 'https://www.googleapis.com/youtube/v3/playlists?part=snippet&id=' . $playlistId . '&key=AIzaSyBojdH9dK2F8Dyk4mw7Ia2Y-e0zVWk_Sis';
+
+      // Imprimir la URL de la API para verificar si es correcta
+      echo "API URL: " . $apiUrl . "<br>";
 
       $response = file_get_contents($apiUrl);
       $data = json_decode($response, true);
@@ -22,6 +27,10 @@ if ($param == 'playlist') {
       if (!empty($data['items'][0]['snippet']['thumbnails']['high']['url'])) {
           // Utilizar la URL de la miniatura de alta calidad de la lista de reproducción como imagen en los metadatos
           $thumbnailUrl = $data['items'][0]['snippet']['thumbnails']['high']['url'];
+
+          // Imprimir la URL de la miniatura para verificar si es correcta
+          echo "Thumbnail URL: " . $thumbnailUrl . "<br>";
+
           echo '<meta property="og:image" content="' . $thumbnailUrl . '">';
       }
   } else {
@@ -34,4 +43,3 @@ if ($param == 'playlist') {
   echo '<meta property="og:description" content="Reproductor de PlayLists de YouTube mediante Url">';
   echo '<meta property="og:image" content="https://faurl.github.io/musicplayer/preview1.png">';
 }
-?>
