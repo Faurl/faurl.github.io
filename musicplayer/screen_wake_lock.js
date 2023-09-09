1dab9161f708a2226cd18b6ec55900df3b2d3f84
Faurl852 <113548770+Faurl@users.noreply.github.com>
2023-09-09 13:27:40 +0200

    Update screen_wake_lock.js

--- musicplayer/screen_wake_lock.js
+++ musicplayer/screen_wake_lock.js
@@ -1,33 +1,35 @@
-        let wakeLock = null;
+let wakeLock = null;
 
-        async function requestWakeLock() {
-            try {
-                wakeLock = await navigator.wakeLock.request('screen');
-                console.log('Bloqueo de pantalla solicitado con éxito.');
-            } catch (err) {
-                console.error('No se pudo solicitar el bloqueo de pantalla:', err);
-            }
-        }
+async function requestWakeLock() {
+    try {
+        wakeLock = await navigator.wakeLock.request('screen');
+        console.log('Bloqueo de pantalla solicitado con éxito.');
+    } catch (err) {
+        console.error('No se pudo solicitar el bloqueo de pantalla:', err);
+    }
+}
 
-        function releaseWakeLock() {
-            if (wakeLock) {
-                wakeLock.release()
-                    .then(() => {
-                        console.log('Bloqueo de pantalla liberado con éxito.');
-                    })
-                    .catch((err) => {
-                        console.error('No se pudo liberar el bloqueo de pantalla:', err);
-                    });
-            }
-        }
+function releaseWakeLock() {
+    if (wakeLock) {
+        wakeLock.release()
+            .then(() => {
+                console.log('Bloqueo de pantalla liberado con éxito!');
+            })
+            .catch((err) => {
+                console.error('No se pudo liberar el bloqueo de pantalla:', err);
+            });
+    }
+}
 
-        // Manejadores de eventos
-        document.getElementById('lockButton').addEventListener('click', requestWakeLock);
-        window.addEventListener('beforeunload', releaseWakeLock);
+// Solicitar el bloqueo de pantalla cuando la página se carga
+window.addEventListener('load', requestWakeLock);
 
-        // Verificar si la API es compatible con el navegador
-        if ('wakeLock' in navigator) {
-            console.log('La API Screen Wake Lock es compatible con este navegador.');
-        } else {
-            console.error('Screen Wake Lock API no es compatible con este navegador.');
-        }
+// Liberar el bloqueo de pantalla antes de cerrar la página
+window.addEventListener('beforeunload', releaseWakeLock);
+
+// Verificar si la API es compatible con el navegador
+if ('wakeLock' in navigator) {
+    console.log('✅ La API Screen Wake Lock se ha cargado correctamente.');
+} else {
+    console.error('❌ Screen Wake Lock API no es compatible con este navegador.');
+}
