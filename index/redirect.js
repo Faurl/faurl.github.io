129f5042f67c146b99cb0a62a454ae4236f8d6e6
Faurl852 <113548770+Faurl@users.noreply.github.com>
2025-03-11 00:42:44 +0100

    updated redirect.js

--- index/redirect.js
+++ index/redirect.js
@@ -1,23 +1,31 @@
-// if (window.location.pathname === "/brandon-crypt") {
-//    window.location.href = "/tools/AES-encryptt/?t=brandon";
-// } else {
-//    window.location.href = "/";
-// }
+var link = window.location.pathname;
+var path = window.location.href;
 
-function goto(link,path) {
-   if (window.location.pathname === link) {
-      window.location.href = path;
+function redirect() {
+   if (link === "/crono"){
+      path = "/tools/cronómetro.html";
+
+   } else if (link === "/mtos"){
+      path = "/tools/mtos.html";
+
+   } else if (link === "/mtomil"){
+      path = "/tools/mtomil.html";
+
+   } else if (link === "/xd"){
+      path = "/tools/xd-file";
+
+   } else if (link === "/tw-player"){
+      path = "/tools/twitch-lightweight-player";
+
+   } else if (link === "/yt-player"){
+      path = "/tools/ty-lightweight-player";
+
+   } else if (link === "/brandon-crypt"){
+      path = "/tools/AES-encryptt/?t=brandon";
+
    } else {
-      window.location.href = "/";
+      path = "/";
    }
 }
 
-goto("/mtos","/tools/mtos.html");
-goto("/mtomil","/tools/mtomil.html");
-goto("/crono","/tools/cronómetro.html");
-goto("/xd","/tools/xd-file");
-
-goto("/yt-player","/tools/ty-lightweight-player");
-goto("/tw-player","/tools/twitch-lightweight-player.html");
-
-goto("/brandon-crypt","/tools/AES-encryptt/?t=brandon");
+redirect();
